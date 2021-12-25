b57fb63d74d3d50d089153921cc55110df5578c5
Alex Cortez <acim650@gmail.com>
2021-12-25 11:34:32 -0800

    Website contact validation done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import useForm from '../../useForm/useForm';
 import './Contact.css';
+import validateInfo from '/Users/l/rhafaelherrera/src/components/validateInfo/validateInfo.js';
+
 
 function Contact() {
+
+    const { handleChange, values, handleSubmit, errors } = useForm(validateInfo)
+
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    function submitForm() {
+        setIsSubmitted(true);
+    }
+
     return (
         <div>
             <div className='contact-container-background'>
@@ -11,17 +23,47 @@ function Contact() {
                             Contact Me
                         </h1>
                     </div>
-                    <div className='input-section'>
-                        <input className='app-form-control' type='text' name='name' id='name' placeholder='Name' />
-                        <input className='app-form-control' type='text' name='email' id='email' placeholder='Email' />
-                        <textarea className='app-form-control' type='submit' name='message' id='message' placeholder='Message' />
+                    <div className='input-section' id='input-section' onSubmit={handleSubmit}>
+                            <div className='input-container'>
+                                <input 
+                                    className='app-form-control' 
+                                    type='text' name='name' 
+                                    id='name' placeholder='Name' 
+                                    value={values.name} 
+                                    onChange={handleChange} />
+                                {errors.name && <p>{errors.name}</p>}
+                            </div>
+                            <div className='input-container'>
+                                <input 
+                                    className='app-form-control' 
+                                    type='text' 
+                                    name='email' 
+                                    id='email' 
+                                    placeholder='Email' 
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    />
+                                {errors.email && <p>{errors.email}</p>}
+                            </div>
+                            <div className='input-container'>
+                                <textarea 
+                                    className='app-form-control' 
+                                    type='submit' 
+                                    name='message' 
+                                    id='message' 
+                                    placeholder='Message'
+                                    value={values.message}
+                                    onChange={handleChange} 
+                                    />
+                                {errors.message && <p>{errors.message}</p>}
+                            </div>
                     </div>
                 </div>
                 <div className='button-container-half'>
                     <div>
                     </div>
                     <div className='button-container'>
-                        <button className='contact-button' type='submit'>
+                        <button className='contact-button' type='submit' onClick={handleSubmit}>
                             Send  →
                         </button>
                     </div>
