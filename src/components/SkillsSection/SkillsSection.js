6b879bfe79e0c78043561657c4421eff9d3c7cbc
Alex Cortez <acim650@gmail.com>
2022-01-02 20:42:35 -0800

    adjusted skill images for mobile

--- src/components/SkillsSection/SkillsSection.js
+++ src/components/SkillsSection/SkillsSection.js
@@ -9,21 +9,21 @@ function SkillsSection() {
         <div>
             <div className='skill-section'>
                 <div className='skill-image-container'>
-                    <div className='skill-image'>
+                    <div className='skill-image-box'>
                         <img src={skillimageOne} className='skill-image' alt='design skills logo' />
                         <h2>Design</h2>
                         <p className='skill-paragraph'>
                             Designs projects with a definite scope and timeline. 
                         </p>
                     </div>
-                    <div className='skill-image'>
+                    <div className='skill-image-box'>
                         <img src={skillimageTwo} className='skill-image' alt='project management logo' />
                         <h2>Management</h2>
                         <p className='skill-paragraph'>
                             Manages projects and oversees operations.
                         </p>
                     </div>
-                    <div className='skill-image'>
+                    <div className='skill-image-box'>
                         <img src={skillimageThree} className='skill-image' alt='software skills logo' />
                         <h2>Software</h2>
                         <p className='skill-paragraph'>
